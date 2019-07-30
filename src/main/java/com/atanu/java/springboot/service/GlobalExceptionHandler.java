b1c7f6ea673323f2717a3d376f2b908276267601
Atanu Bhowmick <mail2atanu007@gmail.com>
2019-07-30 15:46:52 +0530

    Global exception handler added

---
+++ src/main/java/com/atanu/java/springboot/service/GlobalExceptionHandler.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.atanu.java.springboot.service;
+
+import org.hibernate.HibernateException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.atanu.java.springboot.constant.Constants;
+import com.atanu.java.springboot.exception.DataSvcException;
+import com.atanu.java.springboot.logger.ApplicationLogger;
+import com.atanu.java.springboot.model.FaultDO;
+import com.atanu.java.springboot.utils.CommonUtils;
+
+/**
+ * @author Atanu Bhowmick
+ *
+ */
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	private static final ApplicationLogger logger = new ApplicationLogger(GlobalExceptionHandler.class);
+
+	@ExceptionHandler(DataSvcException.class)
+	public ResponseEntity<FaultDO> handleDataSvcException(DataSvcException ex) {
+		logger.error("Handling DataSvcException... ", ex);
+		FaultDO fault = CommonUtils.createFaultDOForError(ex.getErrorCode(), ex.getErrorMsg());
+		return new ResponseEntity<>(fault, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(HibernateException.class)
+	public ResponseEntity<FaultDO> handleHibernateExceptionException(HibernateException ex) {
+		logger.error("Handling HibernateException... ", ex);
+		FaultDO fault = CommonUtils.createFaultDOForError(Constants.ERROR_CODE_2001, ex.getMessage());
+		return new ResponseEntity<>(fault, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(TransactionException.class)
+	public ResponseEntity<FaultDO> handleJDBCConnectionExceptionException(TransactionException ex) {
+		logger.error("Handling TransactionException... ", ex);
+		FaultDO fault = CommonUtils.createFaultDOForError(Constants.ERROR_CODE_2005, ex.getMessage());
+		return new ResponseEntity<>(fault, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<FaultDO> handleGenericException(Exception ex) {
+		logger.error("Handling Exception... ", ex);
+		FaultDO fault = CommonUtils.createFaultDOForError(Constants.ERROR_CODE_2005, Constants.ERROR_MSG_2005);
+		return new ResponseEntity<>(fault, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
