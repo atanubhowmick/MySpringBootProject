a0556ce8ceea5593f50f8c7373088d3ee550c6c3
Atanu Bhowmick <mail2atanu007@gmail.com>
2019-08-27 00:27:39 +0530

    Eureka client added

--- src/main/java/com/atanu/java/springboot/app/DataSvcSpringBootApp.java
+++ src/main/java/com/atanu/java/springboot/app/DataSvcSpringBootApp.java
@@ -13,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -27,6 +28,7 @@ import com.atanu.java.springboot.logger.ApplicationLogger;
  *
  */
 
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = { "com.atanu.java.springboot" })
 @EnableAutoConfiguration(exclude = {
         DataSourceAutoConfiguration.class,
